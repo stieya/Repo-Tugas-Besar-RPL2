049f1939b332771b141cbbeec798be89a3baa362
ismailzakky <ismailzakky@yahoo.com>
2014-07-06 10:50:29 +0700

    sikil

--- baru/database/emagang.sql
+++ baru/database/emagang.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.1.72-community : Database - emagang
+MySQL - 5.5.24-log : Database - emagang
 *********************************************************************
 */
 
@@ -27,11 +27,9 @@ CREATE TABLE `t_comment_list` (
   `isi_comment` varchar(100) DEFAULT NULL,
   `waktu_comment` datetime DEFAULT NULL,
   PRIMARY KEY (`id_comment_list`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_comment_list` */
-
-insert  into `t_comment_list`(`id_comment_list`,`id_job_list`,`id_user`,`isi_comment`,`waktu_comment`) values (1,4,1,'Coba dikit komen','2014-06-21 08:58:20'),(2,4,1,'awdawd','2014-06-21 10:43:28'),(3,4,2,'Hello','2014-06-21 10:55:57'),(8,4,1,'Coba dong di coba coba biar enak ayo di coba','2014-06-21 08:54:37'),(7,4,1,'coba woy','2014-06-21 08:49:14'),(9,3,1,'yang ini juga enakan di cobain','2014-06-21 08:55:05'),(10,3,1,'','2014-06-21 08:57:07'),(11,3,1,'validasinya belum ada\r\npke capcay anti spam','2014-06-21 04:45:04'),(12,4,2,'Halo semuanya','2014-06-21 05:53:48'),(13,4,1,'0','2014-06-23 12:18:57'),(14,4,1,'dawdawd','2014-06-23 12:20:19');
 
 /*Table structure for table `t_job_list` */
 
@@ -45,11 +43,9 @@ CREATE TABLE `t_job_list` (
   `file_perusahaan` varchar(100) DEFAULT NULL,
   `status` enum('Finished','Unclaimed') DEFAULT NULL,
   PRIMARY KEY (`id_job_list`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_job_list` */
-
-insert  into `t_job_list`(`id_job_list`,`id_job_sheet`,`head`,`body`,`file_perusahaan`,`status`) values (4,6,'Dibuat enak lagi','Pengen dienakin','file/TKELAS.xlsx','Finished'),(3,6,'Enakin aja','Diusahakan enak',NULL,'Unclaimed'),(5,12,'Coba lagi','Isinya apa ya bingung',NULL,'Finished');
 
 /*Table structure for table `t_job_sheet` */
 
@@ -66,11 +62,9 @@ CREATE TABLE `t_job_sheet` (
   `status` enum('Ongoing','Finished','Unclaimed','Hidden') NOT NULL,
   `durasi` int(11) NOT NULL,
   PRIMARY KEY (`id_job_sheet`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_job_sheet` */
-
-insert  into `t_job_sheet`(`id_job_sheet`,`id_perusahaan`,`nama_job_sheet`,`deskripsi_job_sheet`,`id_jurusan`,`tanggal_posting`,`tanggal_akhir`,`status`,`durasi`) values (12,1,'Bikin anak tetangga','Ahh gitu kan',1,'2014-06-21 01:15:55','2014-06-22 01:15:58','Finished',13),(13,7,'awdawdawdawdwdad','Deskripsi Job Sheet',0,'2014-06-23 11:12:04',NULL,'Unclaimed',1),(6,1,'Bikin Lemper Panjang','Deskripsi Job Sheet Deskripsi Job Sheet  Deskripsi Job Sheet',5,'2014-06-17 04:21:01','2014-06-27 16:40:12','Hidden',1),(10,1,'Bikin enak anak orang','Buka dikit josss',3,'2014-06-20 20:16:02','2014-06-20 20:16:08','Ongoing',12),(11,1,'Bikin enak istri orang','Wedew masbrow',4,'2014-06-21 01:14:26','2014-06-22 01:14:30','Unclaimed',16);
 
 /*Table structure for table `t_job_sheet_application` */
 
@@ -85,11 +79,9 @@ CREATE TABLE `t_job_sheet_application` (
   `status` enum('1','0') DEFAULT NULL,
   `comment` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_job_sheet_application`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_job_sheet_application` */
-
-insert  into `t_job_sheet_application`(`id_job_sheet_application`,`id_job_sheet`,`id_student`,`application_file`,`waktu_daftar`,`status`,`comment`) values (7,6,1,'files/student/1/3-EVALUASI_KEAMANAN_SISTEM_INFORMASI_1.pdf','2014-06-25 13:58:49','0',' awdaw'),(8,6,1,'files/student/1/4-MENGAMANKAN_SISTEM_INFORMASI_1.pdf','2014-06-25 14:31:50','0','0'),(9,6,1,'files/student/1/4-MENGAMANKAN_SISTEM_INFORMASI_2.pdf','2014-06-25 14:33:01','0','0'),(10,6,1,'files/student/1/3-EVALUASI_KEAMANAN_SISTEM_INFORMASI_2.pdf','2014-06-25 14:37:55','0','0'),(11,3,1,'awdawd','2014-06-30 16:38:00','0','awdad');
 
 /*Table structure for table `t_jurusan` */
 
@@ -99,11 +91,11 @@ CREATE TABLE `t_jurusan` (
   `id_jurusan` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_jurusan`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_jurusan` */
 
-insert  into `t_jurusan`(`id_jurusan`,`nama`) values (1,'Teknik Informatika'),(2,'Manajemen Informatika'),(3,'Akutansi'),(4,'Desain Komunikasi Visual'),(5,'Teknik Komputer'),(6,'Teknik Elektro'),(7,'Teknik Industri'),(8,'Matematika'),(9,'Fisika'),(10,'Ilmu Pemerintahan'),(11,'Komunikasi'),(12,'Psikologi'),(13,'Kedokteran'),(14,'Hukum'),(16,'cobabca');
+insert  into `t_jurusan`(`id_jurusan`,`nama`) values (17,'Psikologi'),(16,'Teknik Informatika');
 
 /*Table structure for table `t_kota` */
 
@@ -114,11 +106,11 @@ CREATE TABLE `t_kota` (
   `id_provinsi` int(11) NOT NULL,
   `nama` varchar(25) NOT NULL,
   PRIMARY KEY (`id_kota`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_kota` */
 
-insert  into `t_kota`(`id_kota`,`id_provinsi`,`nama`) values (1,1,'Bandung'),(2,1,'Cimohay'),(3,2,'Serang'),(4,3,'Semarang'),(5,2,'Tangerang'),(6,2,'Cilegon'),(7,0,'COba'),(8,4,'Bar');
+insert  into `t_kota`(`id_kota`,`id_provinsi`,`nama`) values (1,1,'Bandung'),(2,1,'Cimohay');
 
 /*Table structure for table `t_message` */
 
@@ -134,11 +126,9 @@ CREATE TABLE `t_message` (
   `tanggal` datetime DEFAULT NULL,
   `status` enum('1','0') DEFAULT NULL,
   PRIMARY KEY (`id_message`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_message` */
-
-insert  into `t_message`(`id_message`,`id_user_pengirim`,`id_user_penerima`,`head`,`body`,`url`,`tanggal`,`status`) values (14,1,2,NULL,'awdawdawd',NULL,'2014-06-30 15:01:14','1');
 
 /*Table structure for table `t_notification` */
 
@@ -153,7 +143,7 @@ CREATE TABLE `t_notification` (
   `tanggal` datetime DEFAULT NULL,
   `status` enum('1','0') DEFAULT NULL,
   PRIMARY KEY (`id_notification`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_notification` */
 
@@ -164,14 +154,14 @@ DROP TABLE IF EXISTS `t_penilaian`;
 CREATE TABLE `t_penilaian` (
   `id_penilaian` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
-  `disiplin` int(11) DEFAULT NULL,
-  `tanggap` int(11) DEFAULT NULL,
-  `cermat` int(11) DEFAULT NULL,
-  `rajin` int(11) DEFAULT NULL,
-  `komunikasi` int(11) DEFAULT NULL,
+  `disiplin` int(50) NOT NULL,
+  `tanggap` int(11) NOT NULL,
+  `cermat` int(11) NOT NULL,
+  `rajin` int(11) NOT NULL,
+  `komunikasi` int(11) NOT NULL,
   `keterangan` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_penilaian`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_penilaian` */
 
@@ -188,12 +178,11 @@ CREATE TABLE `t_perusahaan` (
   `telepon` varchar(13) NOT NULL,
   `id_user` int(11) NOT NULL,
   `website` varchar(50) DEFAULT NULL,
+  `about` text,
   PRIMARY KEY (`id_perusahaan`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_perusahaan` */
-
-insert  into `t_perusahaan`(`id_perusahaan`,`nama`,`alamat`,`id_kota`,`kode_pos`,`telepon`,`id_user`,`website`) values (1,'Perusahaan WOy','asd',1,'14045','080989999',1,NULL),(6,'Perusahaan B','asdasdasdsad',2,'140455','080989999',6,NULL),(5,'Perusahaan B','asdasdasdsad',3,'140455','080989999',5,NULL),(7,'awad','awdaw',1,'123122','123123123',3,NULL);
 
 /*Table structure for table `t_provinsi` */
 
@@ -203,11 +192,11 @@ CREATE TABLE `t_provinsi` (
   `id_provinsi` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id_provinsi`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_provinsi` */
 
-insert  into `t_provinsi`(`id_provinsi`,`nama`) values (1,'Jawa Barat'),(2,'Banten'),(3,'Jawa Tengah'),(4,'Jawa Timur');
+insert  into `t_provinsi`(`id_provinsi`,`nama`) values (1,'Jawa Barat');
 
 /*Table structure for table `t_student` */
 
@@ -226,11 +215,9 @@ CREATE TABLE `t_student` (
   `email` varchar(50) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_student`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_student` */
-
-insert  into `t_student`(`id_student`,`id_universitas`,`nim`,`id_jurusan`,`nama`,`alamat`,`id_kota`,`kode_pos`,`telepon`,`email`,`id_user`) values (1,1,'10111078',1,'Handoyo','Sekeloa Utara',1,'42112','087871942562','dyo.9913@gmail.com',1),(2,2,'1231233',7,'Cinta','Cimahi',2,'42311','0823123323','cinta@gmail.com',2);
 
 /*Table structure for table `t_student_job_list` */
 
@@ -244,11 +231,9 @@ CREATE TABLE `t_student_job_list` (
   `waktu_upload_file` datetime DEFAULT NULL,
   `keterangan` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_student_job_list`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_student_job_list` */
-
-insert  into `t_student_job_list`(`id_student_job_list`,`id_job_list`,`id_student`,`file_user`,`waktu_upload_file`,`keterangan`) values (1,3,1,'files/student/1/1-PENDAHULUAN.pdf','2014-06-25 14:41:44',NULL),(4,4,1,NULL,'2014-06-21 10:31:03',NULL),(5,5,2,NULL,NULL,NULL),(6,4,2,NULL,'2014-06-21 05:54:03',NULL);
 
 /*Table structure for table `t_student_job_sheet` */
 
@@ -263,11 +248,9 @@ CREATE TABLE `t_student_job_sheet` (
   `status` enum('1','0') DEFAULT NULL,
   `keterangan` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_student_job_sheet`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_student_job_sheet` */
-
-insert  into `t_student_job_sheet`(`id_student_job_sheet`,`id_job_sheet`,`id_student`,`waktu_start`,`waktu_akhir`,`status`,`keterangan`) values (1,6,1,'2014-06-20 16:44:01','2014-06-21 16:44:04','1',NULL),(2,10,1,'2014-06-20 20:16:52','2014-06-20 20:16:55','1',NULL),(3,11,1,'2014-06-21 01:16:38','2014-06-22 01:16:41','1',NULL),(4,12,2,'2014-06-21 01:16:50','2014-06-21 01:16:53','1',NULL),(5,6,2,'2014-05-02 00:53:13','2014-06-08 00:53:18','1',NULL),(6,NULL,NULL,NULL,NULL,NULL,NULL);
 
 /*Table structure for table `t_universitas` */
 
@@ -282,11 +265,11 @@ CREATE TABLE `t_universitas` (
   `telepon` varchar(13) DEFAULT NULL,
   `website` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_universitas`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_universitas` */
 
-insert  into `t_universitas`(`id_universitas`,`nama`,`alamat`,`id_kota`,`kode_pos`,`telepon`,`website`) values (1,'UNIKOM','Dipatiukur No 204-209',1,'42112','0223223321','http://unikom.ac.id'),(2,'UNPAD','Dipatiukur',1,'42123','0221231233','http://unpad.ac.id'),(3,'UPH','Karawaci',5,'42032','0213212331','http://uph.ac.id'),(10,'awdawd','0',1,'12312','12312','awdawd');
+insert  into `t_universitas`(`id_universitas`,`nama`,`alamat`,`id_kota`,`kode_pos`,`telepon`,`website`) values (1,'Unikom','Dipatiukur\r\n',1,'14045','123213','wewewewew');
 
 /*Table structure for table `t_user` */
 
@@ -302,11 +285,26 @@ CREATE TABLE `t_user` (
   `last_login` datetime DEFAULT NULL,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `unique` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 /*Data for the table `t_user` */
 
-insert  into `t_user`(`id_user`,`email`,`password`,`foto_user`,`status_user`,`tanggal_masuk`,`last_login`) values (1,'dyo.9913@gmail.com','57ba172a6be125cca2f449826f9980ca','','STUDENT','2014-06-19 05:01:36','2014-06-30 08:29:59'),(2,'cinta@gmail.com','57ba172a6be125cca2f449826f9980ca','images/student/2/bb.jpg','STUDENT','2014-06-19 07:07:36','2014-06-30 15:01:24'),(3,'waw@dad.com','57ba172a6be125cca2f449826f9980ca',NULL,'COMPANY','2014-06-23 11:03:59',NULL),(4,'admin@gmail.com','57ba172a6be125cca2f449826f9980ca',NULL,'ADMIN','2014-06-28 06:40:11','2014-06-30 13:19:27');
+/* Trigger structure for table `t_job_sheet_application` */
+
+DELIMITER $$
+
+/*!50003 DROP TRIGGER*//*!50032 IF EXISTS */ /*!50003 `tri_notif_company_pelamaran_jobsheet` */$$
+
+/*!50003 CREATE */ /*!50017 DEFINER = 'root'@'localhost' */ /*!50003 TRIGGER `tri_notif_company_pelamaran_jobsheet` BEFORE INSERT ON `t_job_sheet_application` FOR EACH ROW insert into t_notification(id_user, head, body, tanggal, status)
+values (
+(select tu.id_user from t_job_sheet tjs join t_job_sheet_application tjsa on (tjs.id_job_sheet = tjsa.id_job_sheet) 	join t_perusahaan tp on (tp.id_perusahaan = tjs.id_perusahaan) join t_user tu on (tu.id_user = tp.id_user) order by 	tjsa.id_job_sheet_application desc limit 1), 
+concat('Pelamaran masuk untuk job sheet ', (select nama_job_sheet from t_job_sheet tjs join t_job_sheet_application tjsa on (tjsa.id_job_sheet = tjs.id_job_sheet) 	where tjs.id_job_sheet = new.id_job_sheet order by id_job_sheet_application desc limit 1)), 
+concat(concat('Pelamaran masuk untuk job sheet ', (select 	nama_job_sheet from t_job_sheet tjs join t_job_sheet_application tjsa on (tjsa.id_job_sheet = tjs.id_job_sheet) where 	tjs.id_job_sheet = new.id_job_sheet order by id_job_sheet_application desc limit 1)), concat(' dari student bernama ', (select 	ts.nama from t_job_sheet_application tjsa join t_student ts on (ts.id_student = tjsa.id_student) where tjsa.id_job_sheet = 	new.id_job_sheet order by id_job_sheet_application desc limit 1))), 
+current_timestamp, 
+0) */$$
+
+
+DELIMITER ;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
